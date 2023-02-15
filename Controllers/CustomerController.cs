6bbe29e1831adb70af3d0c31ddb0069f6a9cbc7e
RikkeHein <94359561+RikkeHein@users.noreply.github.com>
2023-02-15 12:50:41 +0100

    nye metoder

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using CustomerService;
 using System.Linq; 
+using System.Collections.Generic;
 
 namespace CustomerService.Controllers;
 
@@ -18,7 +19,30 @@ public class CustomerController : ControllerBase
         City = "Hørning",
         TaxNumber = "DK-75627732",
         ContactName = "Dennis Jørgensen"
-} };
+},
+
+ new Customer() {
+        Id = 2,
+        Name = "ABC Corporation A/S",
+        Address1 = "Nydamsvej 10",
+        Address2 = null,
+        PostalCode = 8362,
+        City = "Hørning",
+        TaxNumber = "DK-75627732",
+        ContactName = "Caroline Jensen"
+},
+new Customer() {
+        Id = 3,
+        Name = "XYZ Industries A/S",
+        Address1 = "Nydamsvej 30",
+        Address2 = null,
+        PostalCode = 8362,
+        City = "Hørning",
+        TaxNumber = "DK-75627732",
+        ContactName = "Mads Sørensen"
+}
+
+ };
     private readonly ILogger<CustomerController> _logger;
 
     public CustomerController(ILogger<CustomerController> logger)
@@ -31,6 +55,26 @@ public Customer Get(int customerId)
 {
     return _customers.Where(c => c.Id == customerId).First();
 }
+
+[HttpGet("GetAllCustomers")]
+public List<Customer> GetAll()
+{
+    return _customers;
+}
+
+[HttpPost("AddCustomer")]
+public IActionResult AddCustomer([FromBody] Customer customer)
+{
+    if (customer == null)
+    {
+        return BadRequest();
+    }
+
+    _customers.Add(customer);
+    return Ok();
+}
+
+
 }
 
 
